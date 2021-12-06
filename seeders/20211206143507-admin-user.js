0c87ef2860ea47939f9fdc13ff73f55373707518
Abdillah <achmdabdil9@gmail.com>
2021-12-06 22:55:31 +0700

    add admin

--- seeders/20211206143507-admin-user.js
+++ seeders/20211206143507-admin-user.js
@@ -10,6 +10,17 @@ module.exports = {
 			up: (queryInterface, Sequelize) => {
 				return queryInterface.bulkInsert('users', [
 					{
+						fullName: 'admin1',
+						email: 'admin1@gmail.com',
+						password: hashedPassword,
+						gender: 'Male',
+						phone: '0888888888888',
+						address: 'Bekasi',
+						status: 'admin',
+						createdAt: new Date(),
+						updatedAt: new Date(),
+					},
+					{
 						fullName: 'admin',
 						email: 'admin@gmail.com',
 						password: hashedPassword,
