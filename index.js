c3597a4f314b598f30d254c1d2032a388d89b5ae
Abdillah <achmdabdil9@gmail.com>
2021-12-06 21:07:51 +0700

    test

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const router = require('./src/routes/index')
+const router = require('./src/routes/index');
 
-const app = express()
+const app = express();
 
-const PORT = 5000
+const PORT = 5000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/v1/', router)
-app.use("/uploads", express.static("uploads"));
+app.use('/api/v1/', router);
+app.use('/uploads', express.static('uploads'));
 
-app.listen(PORT, () => console.log(`server listening on port : ${PORT}`))
+app.listen(process.env.PORT || PORT, () =>
+	console.log(`server listening on port : ${PORT}`)
+);
