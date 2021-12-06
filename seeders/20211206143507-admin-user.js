f9b7c9a5ba5cc9543577c4ca7576b74de17aa45f
Abdillah <achmdabdil9@gmail.com>
2021-12-06 23:04:46 +0700

    add admin

--- seeders/20211206143507-admin-user.js
+++ seeders/20211206143507-admin-user.js
@@ -5,55 +5,20 @@ const bcrypt = require('bcrypt');
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
 		const hashedPassword = await bcrypt.hash('123123', 10);
-
-		module.exports = {
-			up: (queryInterface, Sequelize) => {
-				return queryInterface.bulkInsert('users', [
-					{
-						fullName: 'admin1',
-						email: 'admin1@gmail.com',
-						password: hashedPassword,
-						gender: 'Male',
-						phone: '0888888888888',
-						address: 'Bekasi',
-						status: 'admin',
-						createdAt: new Date(),
-						updatedAt: new Date(),
-					},
-					{
-						fullName: 'admin',
-						email: 'admin@gmail.com',
-						password: hashedPassword,
-						gender: 'Male',
-						phone: '0888888888888',
-						address: 'Bekasi',
-						status: 'admin',
-						createdAt: new Date(),
-						updatedAt: new Date(),
-					},
-				]);
+		await queryInterface.bulkInsert('users', [
+			{
+				fullName: 'admin',
+				email: 'admin@gmail.com',
+				password: hashedPassword,
+				phone: '0888888888888',
+				address: 'Bekasi',
+				status: 'admin',
+				createdAt: new Date(),
+				updatedAt: new Date(),
 			},
-			down: (queryInterface, Sequelize) => {
-				return queryInterface.bulkDelete('users', null, {});
-			},
-		};
-		/**
-		 * Add seed commands here.
-		 *
-		 * Example:
-		 * await queryInterface.bulkInsert('People', [{
-		 *   name: 'John Doe',
-		 *   isBetaMember: false
-		 * }], {});
-		 */
+		]);
 	},
-
-	down: async (queryInterface, Sequelize) => {
-		/**
-		 * Add commands to revert seed here.
-		 *
-		 * Example:
-		 * await queryInterface.bulkDelete('People', null, {});
-		 */
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete('users', null, {});
 	},
 };
