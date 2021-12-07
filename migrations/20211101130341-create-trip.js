25d4e3057b7ae7aaee3ca7df9af62c309fbab08c
Abdillah <achmdabdil9@gmail.com>
2021-12-07 15:01:08 +0700

    done cloudinary

--- migrations/20211101130341-create-trip.js
+++ migrations/20211101130341-create-trip.js
@@ -1,69 +1,69 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('trips', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      idCountry: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "countries",
-          key: "id",
-        },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      },
-      accomodation: {
-        type: Sequelize.STRING
-      },
-      transportation: {
-        type: Sequelize.STRING
-      },
-      eat: {
-        type: Sequelize.STRING
-      },
-      day: {
-        type: Sequelize.STRING
-      },
-      night: {
-        type: Sequelize.STRING
-      },
-      dateTrip: {
-        type: Sequelize.DATE
-      },
-      price: {
-        type: Sequelize.INTEGER
-      },
-      quota: {
-        type: Sequelize.INTEGER
-      },
-      quotaFilled: {
-        type: Sequelize.INTEGER
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      images: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('trips');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('trips', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			title: {
+				type: Sequelize.STRING,
+			},
+			idCountry: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: 'countries',
+					key: 'id',
+				},
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			},
+			accomodation: {
+				type: Sequelize.STRING,
+			},
+			transportation: {
+				type: Sequelize.STRING,
+			},
+			eat: {
+				type: Sequelize.STRING,
+			},
+			day: {
+				type: Sequelize.STRING,
+			},
+			night: {
+				type: Sequelize.STRING,
+			},
+			dateTrip: {
+				type: Sequelize.DATE,
+			},
+			price: {
+				type: Sequelize.INTEGER,
+			},
+			quota: {
+				type: Sequelize.INTEGER,
+			},
+			quotaFilled: {
+				type: Sequelize.INTEGER,
+			},
+			description: {
+				type: Sequelize.STRING,
+			},
+			images: {
+				type: Sequelize.TEXT,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('trips');
+	},
 };
--- src/controller/trip.js
+++ src/controller/trip.js
@@ -1,244 +1,215 @@
-const { trip, country } = require('../../models')
-const Joi = require('joi')
+const { trip, country } = require('../../models');
+const cloudinary = require('../thirdparty/cloudinary');
 
-exports.addTrip =  async (req, res) => {
-    
-    // // validation schema
-    // const schema = Joi.object({
-    //     title: Joi.string().min(5).required(),
-    //     idCountry: Joi.number().required(),
-    //     accomodation: Joi.string().min(6).required(),
-    //     transportation: Joi.string().min(6).required(),
-    //     eat: Joi.string().min(5).required(),
-    //     day: Joi.string().required(),
-    //     night: Joi.string().required(),
-    //     dateTrip: Joi.date().required(),
-    //     price: Joi.number().required(),
-    //     quota: Joi.number().required(),
-    //     description: Joi.string().min(5).required(),images: Joi.string().required()
-    
-    // })
+exports.addTrip = async (req, res) => {
+	try {
+		const tripExist = await trip.findOne({
+			where: {
+				title: req.body.title,
+			},
+			attributes: {
+				exclude: ['createdAt', 'updatedAt'],
+			},
+		});
 
-    // const { error } = schema.validate(req.body)
+		if (tripExist) {
+			res.status(400).send({
+				status: 'failed',
+				messages: 'trip already exist',
+			});
+			return;
+		}
+		const { images } = req.files;
 
-    // if(error) {
-    //     return res.status(400).send({
-    //         error: {
-    //             message: error.details[0].message
-    //         }
-    //     })
-    // }
+		let imagesURL = [];
 
-    try {
-        const tripExist = await trip.findOne({
-            where: {
-                title: req.body.title,
-            },
-            attributes: {
-                exclude: ["createdAt", "updatedAt"],
-            },
-            });
-        
-        if(tripExist){
-            res.status(400).send({
-                status: "failed",
-                messages: "trip already exist"
-            })
-            return;
-        }
+		for (let image of images) {
+			const result = await cloudinary.uploader.upload(image.path, {
+				use_filename: true,
+				folder: 'dewe-tour-trips',
+			});
+			imagesURL.push(result.url);
+		}
+		imagesURL = JSON.stringify(imagesURL);
 
-        const { images } = req.files;
-        const allImages = images.map(img => img.filename)
-        const allImagesString = JSON.stringify(allImages)
+		const newTrip = await trip.create({
+			...req.body,
+			images: imagesURL,
+		});
 
-        const showImagesUrl = images.map(img => `http://localhost:5000/uploads/trip/${img.filename}`)
+		let showTrip = await trip.findOne({
+			where: {
+				id: newTrip.id,
+			},
+			include: {
+				model: country,
+				as: 'country',
+				attributes: {
+					exclude: ['createdAt', 'updatedAt'],
+				},
+			},
+			attributes: {
+				exclude: ['createdAt', 'updatedAt', 'idCountry'],
+			},
+		});
 
-        const newTrip = await trip.create({
-            ...req.body,
-            images: allImagesString
-        })
+		showTrip = JSON.parse(JSON.stringify(showTrip));
+		showTrip.images = JSON.parse(showTrip.images);
 
-        let showTrip = await trip.findOne({
-            where : {
-                id: newTrip.id
-            },
-            include: {
-                model: country,
-                as: 'country',
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt']
-                }
-            },
-            attributes: {
-                exclude: ['createdAt', 'updatedAt', 'idCountry']
-            }
-        })
-        showTrip = JSON.parse(JSON.stringify(showTrip))
-                  
-        res.status(200).send({
-            status: 'success',
-            messages:'trip succesfully added',
-            data: {
-                ...showTrip,
-                images: showImagesUrl
-            },
-        })
-        } catch (error) {
-            console.log(error)
-            res.status(500).send({
-                status: 'failed',
-                message: 'Server Error'
-            })
-    }
-}
+		res.status(200).send({
+			status: 'success',
+			messages: 'trip succesfully added',
+			data: showTrip,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({
+			status: 'failed',
+			message: 'Server Error',
+		});
+	}
+};
 
 exports.getTrips = async (req, res) => {
-    try {
-        const trips = await trip.findAll({
-            include: {
-                model: country,
-                as: 'country',
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt']
-                }
-            },
+	try {
+		let trips = await trip.findAll({
+			include: {
+				model: country,
+				as: 'country',
+				attributes: {
+					exclude: ['createdAt', 'updatedAt'],
+				},
+			},
 
-            attributes: {
-                exclude: ["createdAt", "updatedAt", "idCountry"],
-            },
-        })
-        trips.map(trip => {
-            const allImages = JSON.parse(trip.images)
+			attributes: {
+				exclude: ['createdAt', 'updatedAt', 'idCountry'],
+			},
+		});
 
-            const newImages = allImages.map(images => `http://localhost:5000/uploads/trip/${images}`)
+		trips.map(trip => {
+			trip.images = JSON.parse(trip.images);
+		});
 
-            trip.images = newImages
-            return trip
-        })
-    
-        res.status(200).send({
-          status: "success",
-          data: trips
-        });
-
-      } catch (error) {
-        console.log(error);
-        res.send({
-          status: "failed",
-          message: "Server Error",
-        });
-    }
-}
+		res.status(200).send({
+			status: 'success',
+			data: trips,
+		});
+	} catch (error) {
+		console.log(error);
+		res.send({
+			status: 'failed',
+			message: 'Server Error',
+		});
+	}
+};
 
 exports.getTrip = async (req, res) => {
-    try {
-        const { id } = req.params;
-    
-        let data = await trip.findOne({
-            where: {
-                id,
-            },
-            include: {
-                model: country,
-                as: 'country',
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt']
-                }
-            },
-            attributes: {
-                exclude: ["createdAt", "updatedAt", "idCountry"],
-            },
-        });
+	try {
+		const { id } = req.params;
 
-        const allImages = JSON.parse(data.images)
+		let data = await trip.findOne({
+			where: {
+				id,
+			},
+			include: {
+				model: country,
+				as: 'country',
+				attributes: {
+					exclude: ['createdAt', 'updatedAt'],
+				},
+			},
+			attributes: {
+				exclude: ['createdAt', 'updatedAt', 'idCountry'],
+			},
+		});
 
-        const newImages = allImages.map(images => `http://localhost:5000/uploads/trip/${images}`)
-        data.images = newImages
-        res.send({
-          status: "success",
-          data
-        });
-      } catch (error) {
-        console.log(error);
-        res.send({
-          status: "failed",
-          message: "Server Error",
-        });
-      }
-}
+		data = JSON.parse(JSON.stringify(data));
+		data.images = JSON.parse(data.images);
+		res.send({
+			status: 'success',
+			data,
+		});
+	} catch (error) {
+		console.log(error);
+		res.send({
+			status: 'failed',
+			message: 'Server Error',
+		});
+	}
+};
 
 exports.updateTrip = async (req, res) => {
-    try {
-        const { id } = req.params
-        await trip.update(req.body, {
-            where: {
-                id
-            }
-        })
+	try {
+		const { id } = req.params;
+		await trip.update(req.body, {
+			where: {
+				id,
+			},
+		});
 
-        const newTrip = await trip.findOne({
-            where: {
-                id
-            },
-            include: {
-                model: country,
-                as: 'country',
-                attributes: {
-                    exclude: ['createdAt', 'updatedAt']
-                }
-            },
-            attributes: {
-                exclude: ["createdAt", "updatedAt", "idCountry"],
-            },
-        })
+		const newTrip = await trip.findOne({
+			where: {
+				id,
+			},
+			include: {
+				model: country,
+				as: 'country',
+				attributes: {
+					exclude: ['createdAt', 'updatedAt'],
+				},
+			},
+			attributes: {
+				exclude: ['createdAt', 'updatedAt', 'idCountry'],
+			},
+		});
 
-        res.status(200).send({
-            status: 'success',
-            data: newTrip
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-          status: "failed",
-          message: "Server Error",
-        });
-    }
-}
+		res.status(200).send({
+			status: 'success',
+			data: newTrip,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).send({
+			status: 'failed',
+			message: 'Server Error',
+		});
+	}
+};
 
 exports.deleteTrip = async (req, res) => {
-    try {
-      const { id } = req.params;
-        
-      const tripExist = await trip.findOne({
-        where: {
-            id,
-        },
-    });
-    
-    if(!tripExist){
-        res.status(400).send({
-            status: "failed",
-            messages: "trip isn't exist"
-        })
-    }
+	try {
+		const { id } = req.params;
 
-      await trip.destroy({
-        where: {
-          id,
-        },
-      });
-  
-      res.send({
-        status: "success",
-        data: {
-            id: `${id}`
-        },
-      });
-    } catch (error) {
-      console.log(error);
-      res.send({
-        status: "failed",
-        message: "Server Error",
-      });
-    }
-}
+		const tripExist = await trip.findOne({
+			where: {
+				id,
+			},
+		});
+
+		if (!tripExist) {
+			res.status(400).send({
+				status: 'failed',
+				messages: "trip isn't exist",
+			});
+		}
+
+		await trip.destroy({
+			where: {
+				id,
+			},
+		});
+
+		res.send({
+			status: 'success',
+			data: {
+				id: `${id}`,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+		res.send({
+			status: 'failed',
+			message: 'Server Error',
+		});
+	}
+};
